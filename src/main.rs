8c87b8fb89f5db9c0048e84006032f674bb05bc9
Robert Ou <rqou@robertou.com>
2017-02-19 21:15:25 -0800

    Hacky parsing out of IP

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,56 @@ use pnetlink::socket::NetlinkProtocol;
 use pnetlink::socket::NetlinkSocket;
 use pnetlink::packet::netlink::NetlinkConnection;
 use pnetlink::packet::netlink::NetlinkReader;
+use pnetlink::packet::route::addr::IFA_ADDRESS;
 use pnetlink::packet::route::addr::RTM_NEWADDR;
+use pnetlink::packet::route::addr::Addr;
 use pnetlink::packet::route::IfAddrPacket;
+use pnetlink::packet::route::RtAttrPacket;
 use pnetlink::packet::route::link::Links;
+
+use std::net::{Ipv4Addr,Ipv6Addr};
+
+// Duplicate all this logic because raisins
+fn nl_align(len: usize) -> usize {
+    const RTA_ALIGNTO: usize = 4;
+
+    ((len)+RTA_ALIGNTO-1) & !(RTA_ALIGNTO-1)
+}
+
+pub struct my_RtAttrIterator<'a> {
+    buf: &'a [u8],
+}
+
+impl<'a> my_RtAttrIterator<'a> {
+    pub fn new(buf: &'a [u8]) -> Self {
+        my_RtAttrIterator {
+            buf: buf,
+        }
+    }
+}
+
+impl<'a> Iterator for my_RtAttrIterator<'a> {
+    type Item = RtAttrPacket<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(rta) = RtAttrPacket::new(&self.buf[..]) {
+            let len = rta.get_rta_len() as usize;
+            if len < 4 {
+                return None;
+            }
+            self.buf = &self.buf[nl_align(len as usize)..];
+            return Some(rta);
+        }
+        None
+    }
+}
 
 use pnet::packet::Packet;
 
 fn main() {
     // FIXME
-    let RTMGRP_IPV4_IFADDR: u32 = 0x10;
-    let RTMGRP_IPV6_IFADDR: u32 = 0x100;
+    const RTMGRP_IPV4_IFADDR: u32 = 0x10;
+    const RTMGRP_IPV6_IFADDR: u32 = 0x100;
 
     let dev_name = std::env::args_os().nth(1).expect("Need a device name!");
     println!("Watching for IP changes on {}...", dev_name.to_string_lossy());
@@ -26,7 +66,6 @@ fn main() {
         // FIXME: Non-UTF-8?
         let link = nl_query_conn.get_link_by_name(&dev_name.to_string_lossy())
             .expect("Device name not found!").expect("Device name not found!");
-        println!("link {:?}", link);
         iface_idx = link.get_index();
     }
 
@@ -36,17 +75,19 @@ fn main() {
     let mut nl_reader = NetlinkReader::new(&mut nl_sock);
 
     while let Ok(Some(pkt)) = nl_reader.read_netlink() {
-        //let pkt = pkt.get_packet();
-
-        println!("{:?}", pkt);
-
         // Only process new addresses (don't bother with deleting)
         if pkt.get_kind() == RTM_NEWADDR {
             if let Some(addrpkt) = IfAddrPacket::new(&pkt.payload()) {
-                println!("ifa {:?}", addrpkt);
-
                 if addrpkt.get_index() == iface_idx {
-                    println!("ASDFASDF");
+                    let iter = my_RtAttrIterator::new(addrpkt.payload());
+                    for rta in iter {
+                        if rta.get_rta_type() == IFA_ADDRESS {
+                            println!("rta {:?}", rta);
+                            let addr = Addr::ip_from_family_and_bytes(
+                                addrpkt.get_family(), rta.payload());
+                            println!("ip {:?}", addr);
+                        }
+                    }
                 }
             }
         }
