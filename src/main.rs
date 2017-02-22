75fae8e612d95d20bdfcb0fa521da05c128c41be
Robert Ou <rqou@robertou.com>
2017-02-21 23:55:26 -0800

    Claenups

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,32 @@ extern crate pnetlink;
 extern crate pnet;
 
 use std::fs::File;
-use std::path::Path;
 use std::io::BufReader;
-// ???
 use std::io::BufRead;
 use std::io::Write;
+use std::path::Path;
+
+use std::net::{Ipv4Addr,Ipv6Addr};
 
 use std::process::Command;
 use std::process::Stdio;
 use std::process::ChildStdin;
 
-use pnetlink::socket::NetlinkProtocol;
-use pnetlink::socket::NetlinkSocket;
 use pnetlink::packet::netlink::NetlinkConnection;
 use pnetlink::packet::netlink::NetlinkReader;
+use pnetlink::packet::route::addr::DEPRECATED;
 use pnetlink::packet::route::addr::IFA_ADDRESS;
 use pnetlink::packet::route::addr::RTM_NEWADDR;
 use pnetlink::packet::route::addr::Addr;
-use pnetlink::packet::route::addr::Scope;
 use pnetlink::packet::route::addr::Addresses;
+use pnetlink::packet::route::addr::Scope;
 use pnetlink::packet::route::addr::IpAddr;
-use pnetlink::packet::route::addr::IfAddrFlags;
 use pnetlink::packet::route::IfAddrPacket;
 use pnetlink::packet::route::RtAttrPacket;
 use pnetlink::packet::route::link::Links;
+use pnetlink::socket::NetlinkProtocol;
+use pnetlink::socket::NetlinkSocket;
 
-use std::net::{Ipv4Addr,Ipv6Addr};
-
-// ???
 use pnet::packet::Packet;
 
 // FIXME
@@ -73,18 +71,18 @@ impl<'a> Iterator for my_RtAttrIterator<'a> {
 
 fn update_ipv4(stdin: &mut ChildStdin, domain: &str, ipaddr: &Ipv4Addr) {
     let delete_str = format!("update delete {} A\n", domain);
-    stdin.write_all(delete_str.as_bytes());
+    stdin.write_all(delete_str.as_bytes()).unwrap();
     let update_str = format!("update add {} 60 A {}\n", domain, ipaddr);
-    stdin.write_all(update_str.as_bytes());
-    stdin.write_all("send\n".as_bytes());
+    stdin.write_all(update_str.as_bytes()).unwrap();
+    stdin.write_all("send\n".as_bytes()).unwrap();
 }
 
 fn update_ipv6(stdin: &mut ChildStdin, domain: &str, ipaddr: &Ipv6Addr) {
     let delete_str = format!("update delete {} AAAA\n", domain);
-    stdin.write_all(delete_str.as_bytes());
+    stdin.write_all(delete_str.as_bytes()).unwrap();
     let update_str = format!("update add {} 60 AAAA {}\n", domain, ipaddr);
-    stdin.write_all(update_str.as_bytes());
-    stdin.write_all("send\n".as_bytes());
+    stdin.write_all(update_str.as_bytes()).unwrap();
+    stdin.write_all("send\n".as_bytes()).unwrap();
 }
 
 fn main() {
@@ -100,28 +98,27 @@ fn main() {
     let mut main_dns_name = String::new();
     let other_dns_names: Vec<((u16, u16, u16, u16), String)>;
 
-    conf_file_reader.read_line(&mut dns_key_file);
-    conf_file_reader.read_line(&mut main_dns_name);
+    conf_file_reader.read_line(&mut dns_key_file).unwrap();
+    conf_file_reader.read_line(&mut main_dns_name).unwrap();
     let dns_key_file = dns_key_file.trim();
     let main_dns_name = main_dns_name.trim();
     other_dns_names = conf_file_reader.lines().map(|l| {
         // WTF?
-        let l = l.unwrap();
-        let l = l.trim();
+        let l: String = l.unwrap();
+        let l: &str = l.trim();
 
         let mut l_split = l.split_whitespace();
         let domain_part = l_split.next().unwrap();
         let addr_part = l_split.next().unwrap();
-        let addr_split: Vec<&str> = addr_part.split(':').collect();
-        let addr_split: Vec<u16> = addr_split.iter()
-            .map(|&x| u16::from_str_radix(x, 16).unwrap()).collect();
+        let addr_split: Vec<u16> = addr_part.split(':')
+            .map(|x| u16::from_str_radix(x, 16).unwrap()).collect();
 
         ((addr_split[0], addr_split[1], addr_split[2], addr_split[3]),
             domain_part.to_string())
     }).collect();
 
     println!("Main domain name: {}", main_dns_name);
-    for (_, other_name) in other_dns_names.to_vec() {
+    for &(_, ref other_name) in &other_dns_names {
         println!("Extra domain name: {}", other_name);
     }
 
@@ -141,22 +138,19 @@ fn main() {
 
         for addr in nl_query_conn.iter_addrs(None).unwrap() {
             if addr.get_link_index() == iface_idx {
-                // Scope global and not deprectated
-                match addr.get_scope() {
-                    Scope::Global => {
-                        if (addr.get_flags().bits() & 0x20) == 0 {
-                            let ip = addr.get_ip().unwrap();
-                            match ip {
-                                IpAddr::V4(v4addr) => {
-                                    initial_v4 = Some(v4addr);
-                                },
-                                IpAddr::V6(v6addr) => {
-                                    initial_v6 = Some(v6addr);
-                                }
+                // Scope global and not deprecated
+                if let Scope::Global = addr.get_scope() {
+                    if (addr.get_flags() & DEPRECATED).bits() == 0 {
+                        let ip = addr.get_ip().unwrap();
+                        match ip {
+                            IpAddr::V4(v4addr) => {
+                                initial_v4 = Some(v4addr);
+                            },
+                            IpAddr::V6(v6addr) => {
+                                initial_v6 = Some(v6addr);
                             }
                         }
-                    },
-                    _ => {}
+                    }
                 }
             }
         }
@@ -174,28 +168,22 @@ fn main() {
     let mut nl_reader = NetlinkReader::new(&mut nl_sock);
 
     // Do initial update
-    match initial_v4 {
-        Some(initial_v4) => {
-            update_ipv4(&mut nsupdate_in, main_dns_name, &initial_v4);
-        },
-        _ => {}
+    if let Some(initial_v4) = initial_v4 {
+        update_ipv4(&mut nsupdate_in, main_dns_name, &initial_v4);
     }
 
-    match initial_v6 {
-        Some(initial_v6) => {
-            update_ipv6(&mut nsupdate_in, main_dns_name, &initial_v6);
+    if let Some(initial_v6) = initial_v6 {
+        update_ipv6(&mut nsupdate_in, main_dns_name, &initial_v6);
 
-            for ((p4, p5, p6, p7), domain) in other_dns_names.to_vec() {
-                let this_addr = Ipv6Addr::new(
-                    initial_v6.segments()[0],
-                    initial_v6.segments()[1],
-                    initial_v6.segments()[2],
-                    initial_v6.segments()[3],
-                    p4, p5, p6, p7);
-                update_ipv6(&mut nsupdate_in, &domain, &this_addr);
-            }
-        },
-        _ => {}
+        for &((p4, p5, p6, p7), ref domain) in &other_dns_names {
+            let this_addr = Ipv6Addr::new(
+                initial_v6.segments()[0],
+                initial_v6.segments()[1],
+                initial_v6.segments()[2],
+                initial_v6.segments()[3],
+                p4, p5, p6, p7);
+            update_ipv6(&mut nsupdate_in, domain, &this_addr);
+        }
     }
 
     while let Ok(Some(pkt)) = nl_reader.read_netlink() {
@@ -218,15 +206,15 @@ fn main() {
                                     update_ipv6(&mut nsupdate_in,
                                         main_dns_name, &v6addr);
 
-                                    for ((p4, p5, p6, p7), domain) in
-                                        other_dns_names.to_vec() {
+                                    for &((p4, p5, p6, p7), ref domain) in
+                                        &other_dns_names {
                                         let this_addr = Ipv6Addr::new(
                                             v6addr.segments()[0],
                                             v6addr.segments()[1],
                                             v6addr.segments()[2],
                                             v6addr.segments()[3],
                                             p4, p5, p6, p7);
-                                        update_ipv6(&mut nsupdate_in, &domain,
+                                        update_ipv6(&mut nsupdate_in, domain,
                                             &this_addr);
                                     }
                                 }
